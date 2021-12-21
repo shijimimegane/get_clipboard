6688f7fb22272cfc20201e2a78f1426b2779b62c
shijimimegane <54845164+shijimimegane@users.noreply.github.com>
2021-12-21 19:57:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
-from io import StringIO
 import pandas as pd
 
+df = pd.DataFrame({"x": [1, 2, 3, 4], "y": ["a", "b", "c", "d"]})
 
-copy_button = Button(label="Get Clipboard Data")
-copy_button.js_on_event("button_click", CustomJS(code="""
-    navigator.clipboard.readText().then(text => document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: text})))
+st.dataframe(df)
+
+copy_button = Button(label="Copy DF")
+copy_button.js_on_event("button_click", CustomJS(args=dict(df=df.to_csv(sep='\t')), code="""
+    navigator.clipboard.writeText(df);
     """))
-result = streamlit_bokeh_events(
+
+no_event = streamlit_bokeh_events(
     copy_button,
     events="GET_TEXT",
     key="get_text",
-    refresh_on_update=False,
+    refresh_on_update=True,
     override_height=75,
     debounce_time=0)
-
-if result:
-    if "GET_TEXT" in result:
-        df = pd.read_csv(StringIO(result.get("GET_TEXT")))
-        st.table(df)
